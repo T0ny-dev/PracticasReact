62f37597dab354e2df1f76a580e2846734963bff
Toni-dev <antonioraweb@gmail.com>
2022-12-05 22:10:41 -0600

    add 01-Hola-Mundo

---
+++ 01-Hola-Mundo/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hola Mundo!</h1>
+    </div>
+  )
+}
+
+export default App
